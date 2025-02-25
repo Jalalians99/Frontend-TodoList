d18ffd17ccbf16aa7601f99f487dcc44fe80179d
Ahmad Jalal <syedmuhammadjalal99@gmail.com>
2025-02-25 10:11:29 +0200

    Added AG react component and added delete funcionality

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,10 +1,40 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Todo } from "../types";
-import TodoTable from "./TodoTable";
+import { AgGridReact } from "ag-grid-react";
+import { AllCommunityModule, ModuleRegistry, ColDef } from "ag-grid-community";
+
+// Register the AllCommunityModule
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 function TodoList() {
-  const [todo, setTodo] = useState<Todo>({ description: "", duedate: "" });
+  const [todo, setTodo] = useState<Todo>({
+    description: "",
+    duedate: "",
+    priority: "",
+  });
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [colDefs] = useState<ColDef[]>([
+    {
+      field: "description",
+      filter: "agTextColumnFilter",
+      headerName: "Description",
+    },
+    {
+      field: "duedate",
+      filter: "agDateColumnFilter",
+      sortable: false,
+      headerName: "Date",
+    },
+    {
+      field: "priority",
+      editable: true,
+      headerName: "Priority",
+      filter: "agTextColumnFilter",
+      cellStyle: (params) =>
+        params.value === "High" ? { color: "red" } : { color: "Black" },
+    },
+  ]);
+  const gridRef = useRef<any>(null);
 
   const handleAdd = () => {
     if (!todo.description) alert("Please enter some values first");
@@ -13,12 +43,21 @@ function TodoList() {
       setTodo({
         description: "",
         duedate: "",
+        priority: "",
       });
     }
   };
 
-  const handleDelete = (row: number) => {
-    setTodos(todos.filter((_, index) => row !== index));
+  const handleDelete = () => {
+    if (gridRef.current.getSelectedNodes().length > 0) {
+      setTodos(
+        todos.filter(
+          (todo, index) => index != gridRef.current.getSelectedNodes()[0].id
+        )
+      );
+    } else {
+      alert("Select a row first!");
+    }
   };
 
   return (
@@ -36,8 +75,24 @@ function TodoList() {
         value={todo.duedate}
         onChange={(e) => setTodo({ ...todo, duedate: e.target.value })}
       />
+      <input
+        type="text"
+        placeholder="Priority"
+        value={todo.priority}
+        onChange={(e) => setTodo({ ...todo, priority: e.target.value })}
+      />
       <button onClick={handleAdd}>Add</button>
-      <TodoTable todos={todos} handleDelete={handleDelete} />
+      <button onClick={handleDelete}>Delete</button>
+      <div style={{ height: 400, width: 600 }} className="ag-theme-alpine">
+        <AgGridReact
+          ref={gridRef}
+          onGridReady={(params) => (gridRef.current = params.api)}
+          rowData={todos}
+          columnDefs={colDefs}
+          rowSelection={"single"}
+          defaultColDef={{ resizable: true }}
+        />
+      </div>
     </>
   );
 }
--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,10 @@
 export type Todo = {
   description: string;
   duedate: string;
+  priority: string;
 };
 
 export type TableProps = {
   todos: Todo[];
-  handleDelete: (row: number) => void;
+  handleDelete: (index: number) => void;
 };
