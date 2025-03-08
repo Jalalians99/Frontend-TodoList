940ceba1716a85f9bce78e09fa13243f56051afb
Ahmad Jalal <syedmuhammadjalal99@gmail.com>
2025-03-08 14:37:11 +0200

    Added Tab and Date picker from MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,90 @@
-import "./App.css"
+
+import React, {useState} from "react"
+import Container from "@mui/material/Container"
+import CssBaseline from "@mui/material/CssBaseline"
 import TodoList  from "./components/TodoList"
+import AppBar from "@mui/material/AppBar"
+import Toolbar from "@mui/material/Toolbar"
+import Typography from "@mui/material/Typography"
+import { Tabs, Tab ,Box } from "@mui/material"
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          {children}
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+function Home() {
+  return <Typography variant="h4">Welcome to the Todo List App</Typography>
+}
+
+function TodoListComponent() {
+  return (
+    <TodoList />
+  );
+}
+
+function SimpleTabs() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div>
+      <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
+        <Tab label="Home" {...a11yProps(0)} />
+        <Tab label="Todo List" {...a11yProps(1)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <Home />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <TodoListComponent />
+      </TabPanel>
+    </div>
+  );
+}
 
 function App() {
   return (
     <>
-      <TodoList />
+    <Container maxWidth="md">
+      <AppBar position="sticky">
+        <Toolbar>
+          <Typography variant="h5" sx={{flexGrow: 1}} >Todo List</Typography>
+        </Toolbar>
+      </AppBar>
+      <CssBaseline />
+      <Box sx={{my: 2}} />
+      <SimpleTabs />
+    </Container>
       </>
   )
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,27 @@
 import { useRef, useState } from "react";
 import { Todo } from "../types";
 import { AgGridReact } from "ag-grid-react";
-import { AllCommunityModule, ModuleRegistry, ColDef } from "ag-grid-community";
+import { AllCommunityModule, ModuleRegistry, ColDef, themeMaterial } from "ag-grid-community";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers';
+import dayjs from 'dayjs';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
+import { Theme, useTheme } from '@mui/material/styles';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import { ThemeProvider } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
+import { autocompleteClasses, NativeSelect } from "@mui/material";
+
+
+
 
 // Register the AllCommunityModule
 ModuleRegistry.registerModules([AllCommunityModule]);
@@ -13,33 +33,50 @@ function TodoList() {
     priority: "",
   });
   const [todos, setTodos] = useState<Todo[]>([]);
+   // Column definitions for the ag-grid
   const [colDefs] = useState<ColDef[]>([
     {
       field: "description",
-      filter: "agTextColumnFilter",
-      floatingFilter: true,
+      // filter: "agTextColumnFilter",
+      // floatingFilter: true,
       headerName: "Description",
     },
     {
       field: "duedate",
-      filter: "agDateColumnFilter",
-      floatingFilter: true,
+      // filter: "agDateColumnFilter",
+      // floatingFilter: true,
       sortable: false,
       headerName: "Date",
     },
-    {
+ {
       field: "priority",
       editable: true,
       headerName: "Priority",
-      filter: "agTextColumnFilter",
-      floatingFilter: true,
+      // filter: "agTextColumnFilter",
+      // floatingFilter: true,
       cellStyle: (params) =>
-        params.value === "High" ? { color: "red" } : { color: "Black" },
+      params.value === "High" ? { color: "red" } : params.value === "Medium" ? { color: "yellow" } : { color: "green" },
     },
   ]);
-  const gridRef = useRef<any>(null);
 
-  const handleAdd = () => {
+
+  // Reference
+  const gridRef = useRef<any>(null);
+  // onChange function for date picker
+  const handleDateChange = (date: any) => {
+    if (date) {
+      setTodo({
+        ...todo,
+        duedate: date.format ("MM/DD/YYYY")});
+      }
+      else {
+        setTodo({
+          ...todo,
+          duedate: ""});
+    }
+  }
+  // Add button function
+    const handleAdd = () => {
     if (!todo.description) alert("Please enter some values first");
     else {
       setTodos([todo, ...todos]);
@@ -50,12 +87,12 @@ function TodoList() {
       });
     }
   };
-
+  // Delete button function
   const handleDelete = () => {
     if (gridRef.current.getSelectedNodes().length > 0) {
       setTodos(
         todos.filter(
-          (todo, index) => index != gridRef.current.getSelectedNodes()[0].id
+          (_, index) => index != Number(gridRef.current?.getSelectedNodes()[0].id)
         )
       );
     } else {
@@ -63,39 +100,79 @@ function TodoList() {
     }
   };
 
+
   return (
     <>
-      <h2>Todo List</h2>
-      <input
-        type="text"
-        placeholder="Description"
+      <Stack direction={"row"} spacing={2}
+        alignItems={"center"} 
+        justifyContent={"center"}
+      > 
+      <TextField
+        variant="outlined"
+        size="medium"
+        label="Description"
         value={todo.description}
         onChange={(e) => setTodo({ ...todo, description: e.target.value })}
       />
-      <input
-        type="date"
-        placeholder="Due Date"
+      {/* <TextField
+        label="Date"
+        size="small"
+        variant="outlined"
         value={todo.duedate}
         onChange={(e) => setTodo({ ...todo, duedate: e.target.value })}
-      />
-      <input
-        type="text"
-        placeholder="Priority"
-        value={todo.priority}
-        onChange={(e) => setTodo({ ...todo, priority: e.target.value })}
-      />
-      <button onClick={handleAdd}>Add</button>
-      <button onClick={handleDelete}>Delete</button>
-      <div style={{ height: 400, width: 600 }} className="ag-theme-alpine">
+        /> */}
+      
+      {/* // Material UI Date Picker */}
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          label="Date"
+          value={todo.duedate ? dayjs(todo.duedate) : null}
+          onChange={(date: dayjs.Dayjs | null) => handleDateChange(date)}
+          slotProps={{ textField: { variant: 'outlined' } }}
+        />
+      </LocalizationProvider>
+
+      <FormControl variant="outlined" size="medium" sx={{ m: 1, minWidth: 170 }}>
+        <InputLabel htmlFor="priority">Priority</InputLabel>
+          <Select
+          value={todo.priority}
+          onChange={(e: SelectChangeEvent) => setTodo({ ...todo, priority: e.target.value })}
+          input={<OutlinedInput label="Priority" id="priority" />}
+        >
+          <MenuItem value={"High"} style={{ color: "red" }}>High</MenuItem>
+          <MenuItem value={"Medium"} style={{ color: "yellow" }}>Medium</MenuItem>
+          <MenuItem value={"Low"} style={{ color: "green" }}>Low</MenuItem>
+        </Select>
+      </FormControl>
+
+
+      
+      <Button 
+          variant="outlined"
+          size="large" 
+          onClick={handleAdd}>Add
+      </Button>
+      <Button 
+          variant="outlined"
+          size="large"
+          color="error" 
+          onClick={handleDelete}>Delete
+      </Button>
+      </Stack>
+      <div style={{ width: '90%', margin: 'auto'}}>
+      <div style={{ height: 400, width: 600 }}  className="ag-theme-alpine">
         <AgGridReact
           ref={gridRef}
+          theme={themeMaterial}
           onGridReady={(params) => (gridRef.current = params.api)}
           rowData={todos}
           columnDefs={colDefs}
           rowSelection={"single"}
           defaultColDef={{ resizable: true }}
         />
+        </div>
       </div>
+      
     </>
   );
 }
